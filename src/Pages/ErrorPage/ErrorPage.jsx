3719131cee5ca48d457a62c103bc0e988b3c0d6b
mahbub-khan <mrkict39@gmail.com>
2024-01-10 02:12:46 +0100

    error page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,22 +1,28 @@
 import { FaHouseChimney } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import Navbar from "../../Components/Shared/Navbar";
+import Footer from "../../Components/Shared/Footer";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center p-5">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold mb-5">🛑 404 🛑</h1>
-        <h1 className="text-5xl font-bold mb-5">
-          We couldn't find your page ☹️
-        </h1>
+    <>
+      <Navbar></Navbar>
+      <div className="min-h-screen flex justify-center items-center p-5">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-5">🛑 404 🛑</h1>
+          <h1 className="text-5xl font-bold mb-5">
+            We couldn't find your page ☹️
+          </h1>
 
-        <Link to="/">
-          <button className="btn btn-active rounded-none bg-transparent">
-            Go to Home <FaHouseChimney />
-          </button>
-        </Link>
+          <Link to="/">
+            <button className="btn btn-active rounded-none bg-transparent">
+              Go to Home <FaHouseChimney />
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+      <Footer></Footer>
+    </>
   );
 };
 
